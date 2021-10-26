a40da00f45cc029069a8bdc903863945744b23de
Himanshu <himanshu.thakur@mobicules.com>
2021-10-26 21:56:08 +0530

    --controller baseurl added

--- src/Modules/Controller/Controller.js
+++ src/Modules/Controller/Controller.js
@@ -1,6 +1,7 @@
 export async function ApiCall(url, payload) {
+    const baseUrl = 'http://44.195.205.114:8080';
     try {
-        let res = await fetch(url, payload);
+        let res = await fetch(baseUrl + url, payload);
         return await res.json();
     } catch (error) {
         console.log(error);
